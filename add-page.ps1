d09f18720dccf62f245afa6c47112bd3b288f40e
amrelgazarm <57614720+amrelgazarm@users.noreply.github.com>
2025-07-12 19:17:48 +1200

    Update add-page.ps1

--- add-page.ps1
+++ add-page.ps1
@@ -1,1 +1,7 @@
-Set-Content -Path "C:\inetpub\wwwroot\index.html" -Value "<html><body><h1>this the VM ONE</h1></body></html>"
+# Install IIS if needed
+if (-not (Get-WindowsFeature -Name Web-Server).Installed) {
+    Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+}
+
+# Create default.html file with your content
+Set-Content -Path "C:\inetpub\wwwroot\default.html" -Value "<html><body><h1>Welcome to VM ONE</h1></body></html>" -Force
